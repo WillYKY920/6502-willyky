e04cec4ce8309f930d89f5c3f8d6e9ec91dcd8a6
WillY <ykywilly@gmail.com>
2026-07-10 22:51:48 +0800

    prog

---
+++ bios.s
@@ -0,0 +1,114 @@
+.setcpu "65C02"
+.debuginfo
+
+.zeropage
+                .org ZP_START0
+READ_PTR:       .res 1
+WRITE_PTR:      .res 1
+
+.segment "SBUFF"
+SBUFF:          .res $100
+
+.segment "BIOS"
+
+ACIA_DATA       = $5000
+ACIA_STATUS     = $5001
+ACIA_CMD        = $5002
+ACIA_CTRL       = $5003
+
+PORTB           = $6000
+PORTA           = $6001
+
+DDRB            = $6002
+DDRA            = $6003
+
+IFR             = $600D ; Interrupt Flag Register
+T1LL            = $6006
+T1LH            = $6007
+LOAD:
+                rts
+
+SAVE:
+                rts
+
+
+; Input a character from the serial interface.
+; On return, carry flag indicates whether a key was pressed
+; If a key was pressed, the key value will be in the A register
+;
+; Modifies: flags, A
+MONRDKEY:
+CHRIN:
+                LDA     ACIA_STATUS
+                AND     #$08
+                BEQ     @rxdelay
+                LDA     ACIA_DATA
+                JSR     CHROUT
+                SEC
+                RTS
+@rxdelay:
+                CLC
+                RTS
+
+
+; Output a character (from the A register) to the serial interface.
+;
+; Modifies: flags
+MONCOUT:
+CHROUT:
+                PHA                    ; Save A.
+                STA     ACIA_DATA      ; Output character.  
+@txdelay:       
+                LDA     ACIA_STATUS
+                AND     #$10           ; check Rx buffer status flag
+                BEQ     @txdelay        ; loop if Tx buffer is not empty
+                PLA                    ; Restore A.
+                RTS                    ; Return.
+
+IRQ_HANDLER:
+                PHA
+                PHX
+                
+                ; 1. Check if a Timer 1 interrupt is actually pending
+                LDA     IFR
+                AND     #$40                ; Check Bit 6 (Timer 1 Flag)
+                BEQ     @skip_sound         ; If T1 didn't cause this, skip sound reload
+                
+                ; 2. Check if Timer 1 interrupts are currently enabled
+                LDA     IER
+                AND     #$40                ; Is Bit 6 enabled in the master register?
+                BEQ     @skip_sound         ; If disabled, skip sound reload entirely!
+
+                ; 3. Handle Legacy Duty-Cycle Audio (For old BEEP/PULSE commands)
+                LDA     PORTB
+                BMI     @duty_high
+
+                ; Load low Phase Duration
+                LDA     DLC
+                STA     T1LL
+                LDA     DLC+1
+                STA     T1LH                ; Writing to T1LH clears the IFR T1 flag
+                JMP     @skip_sound    
+
+@duty_high:      ; Load High Phase Duration 
+                LDA     DHC
+                STA     T1LL
+                LDA     DHC+1
+                STA     T1LH  
+
+@skip_sound:
+                ; --- FUTURE EXPANSION ---
+                ; (This is where you can safely add Keyboard or Serial IRQ checks later!)
+
+@irq_exit: 
+                PLX
+                PLA
+                RTI
+
+.include "wozmon.s"
+.segment "RESETVEC"
+
+.word   $0F00           ; NMI vector
+.word   RESET           ; RESET vector
+.word   IRQ_HANDLER     ; IRQ vector
+
---
+++ sound.s
@@ -0,0 +1,188 @@
+.segment "CODE"
+.ifdef EATER
+
+T1CL            = $6004
+T1CH            = $6005
+
+ACR             = $600B
+IER             = $600E
+
+BEEP: ; BEEP(period, duration)
+                JSR     FRMEVL
+                JSR     MKINT
+                ; Check if parameter is zero (frequency in Hz)
+                LDA     FAC+4
+                ORA     FAC+3
+                BEQ     DURATION
+
+                ; Convert uS to 2MHz CPU Cycles: Multiplied by 2
+                ASL     FAC+4   ; Shift low byte left
+                ROL     FAC+3   ; Rotate high byte left with carry
+
+                ; Calculate timer count for the desired frequency
+                LDA     FAC+4
+                STA     T1CL
+                LDA     FAC+3
+                STA     T1CH
+                ; Generate a square wave on pin PB7
+                LDA     #$C0
+                STA     ACR
+
+                JMP     DURATION ; get second parameter (duration in ms)
+
+SYNTH: ; SYNTH(V1_HalfPeriod, V2_HalfPeriod, Duration)
+                JSR     FRMEVL  ; Parse first parameter (Voice 1)
+                JSR     MKINT
+                
+                ; Check if Voice 1 is 0 (rest)
+                LDA     FAC+4
+                ORA     FAC+3
+                BEQ     @skip_v1
+
+                ; Convert uS to 2MHz CPU Cycles: Multiply by 2
+                ASL     FAC+4   
+                ROL     FAC+3   
+
+                ; Load Timer 1 
+                LDA     FAC+4
+                STA     T1CL
+                LDA     FAC+3
+                STA     T1CH
+
+                ; Enable T1 Continuous Square Wave on PB7
+                LDA     #$C0
+                STA     ACR
+                JMP     @parse_v2
+
+@skip_v1:
+                LDA     #$00
+                STA     ACR     ; Disable PB7 hardware toggle if V1 is 0
+
+@parse_v2:
+                JSR     CHKCOM  ; Check for comma separating params
+                JSR     FRMEVL  ; Parse second parameter (Voice 2)
+                JSR     MKINT
+                
+                ; Check if Voice 2 is 0
+                LDA     FAC+4
+                ORA     FAC+3
+                BEQ     DURATION
+
+                ; Convert uS to 2MHz CPU Cycles: Multiply by 2 
+                ASL     FAC+4
+                ROL     FAC+3
+
+                ; Store Voice 2 values in temporary zero-page memory (reusing DLC)
+                LDA     FAC+4
+                STA     DLC
+                LDA     FAC+3
+                STA     DLC+1
+
+                ; Set PB6 to Output Mode in DDRB (Address $6002)
+                LDA     $6002
+                ORA     #$40
+                STA     $6002
+
+                ; Load Timer 2 Latch (T2CL = $6008, T2CH = $6009)
+                LDA     DLC
+                STA     $6008
+                LDA     DLC+1
+                STA     $6009
+
+DURATION:
+                JSR     CHKCOM  ; Check for final comma
+                JSR     GETBYT  ; Duration placed into X register
+                
+                CPX     #$00
+                BEQ     DONE
+                
+@delay1:
+                LDY     #$FF
+@delay2:
+                ; --- SOFTWARE POLL FOR TIMER 2 (VOICE 2 on PB6) ---
+                LDA     $600D   ; Read Interrupt Flag Register (IFR)
+                AND     #$20    ; Check Timer 2 flag (Bit 5)
+                BEQ     @no_t2_toggle
+                
+                ; Timer 2 triggered! Toggle PB6
+                LDA     $6000   ; Read ORB
+                EOR     #$40    ; Flip Bit 6
+                STA     $6000   ; Write ORB
+                
+                ; Reload T2CH to clear the IFR flag and restart Timer 2
+                LDA     DLC+1
+                STA     $6009
+                
+@no_t2_toggle:
+                ; Delay padding to perfectly match your original loop's timing
+                ; This ensures the 2.17 division in the Python script remains perfectly accurate
+                NOP
+                NOP
+
+                DEY
+                BNE     @delay2
+                DEX
+                BNE     @delay1        
+
+DONE:           
+                ; Stop Timer 1 square wave and reset ACR
+                LDA     #$00
+                STA     ACR
+                
+                ; Pull PB6 Low to prevent DC drain on the speaker
+                LDA     $6000
+                AND     #$BF
+                STA     $6000
+                
+                ; Reset IER (From your original file)
+                LDA     #$40
+                STA     IER
+                
+                RTS
+
+PULSE: ; PULSE(t_DutyHigh, t_DutyLow)
+                JSR     FRMEVL  ; parse first parameter
+                JSR     MKINT
+                ; Check if first parameter (t_dutyHigh) is zero 
+                LDA     FAC+4
+                ORA     FAC+3
+                BEQ     DONE
+
+                ASL     FAC+4   ; Shift low byte left
+                ROL     FAC+3   ; Rotate high byte left with carry
+
+                ; Calculate timer count for the desired frequency
+                SEC             ; Set carry flag before subtraction
+                LDA     FAC+4   ; Load low byte
+                SBC     #2      ; Subtract 2
+                STA     DLC+1
+                LDA     FAC+3
+                STA     DLC
+
+                JSR     CHKCOM  ; check for second parameter (t_dutyLow)
+                JSR     FRMEVL  ; parse second parameter
+                JSR     MKINT   ; convert second parameter to integer
+                
+                ASL     FAC+4   ; Shift low byte left
+                ROL     FAC+3   ; Rotate high byte left with carry
+
+                SEC             ; Set carry flag before subtraction
+                LDA     FAC+4   ; Load low byte
+                SBC     #2      ; Subtract 2 
+                STA     DHC+1
+                LDA     FAC+3
+                STA     DHC
+
+                ; Generate a square wave on pin PB7
+                LDA     #$C0
+                STA     ACR
+                STA     IER
+
+                LDA     DLC+1
+                STA     T1CL
+                LDA     DLC
+                STA     T1CH
+
+                RTS
+
+.endif
